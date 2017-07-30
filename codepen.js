a72290bf257a1f8ec838a6e9e7f59c63b0b387bd
Marco Seguri <seguri@users.noreply.github.com>
2017-07-30 14:51:51 +0200

    Add functions to generate random numbers

--- codepen.js
+++ codepen.js
@@ -1,3 +1,13 @@
+/**
+ * See
+ * https://stackoverflow.com/questions/894860/set-a-default-parameter-value-for-a-javascript-function
+ * https://stackoverflow.com/questions/1527803/generating-random-whole-numbers-in-javascript-in-a-specific-range
+ */
+
+/**
+ * '%@ %@'.fmt('foo', 'bar') returns 'foo bar'
+ * '%@2 %@1'.fmt('foo', 'bar') returns 'bar foo'
+ */
 String.prototype.fmt = function (/* arg1, arg2, ..., argn */) {
     var idx = 0, // current index for non-numerical replacements
         formats = Array.prototype.slice.call(arguments);
@@ -8,4 +18,29 @@ String.prototype.fmt = function (/* arg1, arg2, ..., argn */) {
         return (match === null ? '(null)' : match === undefined ? '' : match).toString();
     });
 };
+
+/**
+ * Returns a random number between min (inclusive) and max (exclusive)
+ */
+function getRandom(min = 0, max = 1) {
+    return Math.random() * (max - min) + min;
+}
+
+/**
+ * Returns a random integer between min (inclusive) and max (exclusive).
+ * Using Math.round() would give you a non-uniform distribution!
+ */
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
+/**
+ * Returns a random integer between min (inclusive) and max (inclusive).
+ * Using Math.round() would give you a non-uniform distribution!
+ */
+function getRandomIntInclusive(min, max) {
+    return getRandomInt(min, max+1);
+}
 
